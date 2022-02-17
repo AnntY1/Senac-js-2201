a724a2e1285594dfeb39f9be1920d3ca04fc40a4
ROBSON FERREIRA HERCULANO JUNIOR <robson.fhjunior@senacsp.edu.br>
2022-02-16 22:11:09 -0300

    Aula 2

---
+++ app.js
@@ -0,0 +1,32 @@
+//Comentario de linha
+
+/*
+comentario de bloco
+*/
+
+const XPTO = 10;
+
+console.log(XPTO);
+
+let minhaVar = "Olá";
+
+minhaVar = minhaVar + "Mundo!";
+
+console.log(minhaVar);
+
+function aula(){
+
+    if (minhaVar = "Olá"){
+        console.log("vc conseguiu");
+    }
+    for(i = 1; i<10; i++){
+    
+        console.log(i);
+    } 
+}
+
+aula();
+
+function diretoNoElemento(){
+    console.log('olá mundo!');
+}
