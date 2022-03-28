090c95c8d0cf4cb74fab2abdc91e9f21bf71b833
unknown <robson_herculano@hotmail.com>
2022-03-27 21:33:54 -0300

    aula/01

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
 comentario de bloco
 */
 
+
+// const - variavel imutavel. Não altera 
+
 const XPTO = 10;
 
 console.log(XPTO);
 
-let minhaVar = "Olá";
 
+
+let minhaVar = "Olá";  // let -  variavel que pode mudar seu estado.
 minhaVar = minhaVar + "Mundo!";
 
 console.log(minhaVar);
@@ -19,7 +23,7 @@ function aula(){
     if (minhaVar = "Olá"){
         console.log("vc conseguiu");
     }
-    for(i = 1; i<10; i++){
+    for(i = 1; i<10; i++){ // imprime um numero de 1 a 9, repetição
     
         console.log(i);
     } 
