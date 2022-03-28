374788abd7e495ad0f82aab89e8017cda0483e40
unknown <robson_herculano@hotmail.com>
2022-03-27 22:09:49 -0300

    aula/05

--- dom3.js
+++ dom3.js
@@ -1,9 +1,9 @@
-const tamanho = 'A:'+ window.innerHeight + 'x L:' + window.innerWidth;
+const tamanho = 'A:'+ window.innerHeight + 'x L:' + window.innerWidth; //saber o tamanho da tela altura e largura.
 
-const navegador = window.navigator;
+const navegador = window.navigator; //informações do navegador. 
 
 console.log(navegador);
 
-const paragrafo = document.querySelector('p');
+const paragrafo = document.querySelector('p'); //pega um elemento do html. Selector pega tudo, paragrafo, lista, etc.
 
-paragrafo.innerText = navegador;
+paragrafo.innerText = navegador; 
