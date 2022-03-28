1b8e539b63583d299740544112c0734ad92d0954
unknown <robson_herculano@hotmail.com>
2022-03-27 21:55:22 -0300

    aula/04

--- dom.js
+++ dom.js
@@ -1,20 +1,20 @@
 
-const p = document.getElementById('paragrafo');
+const p = document.getElementById('paragrafo'); //busca o id do html refente a paragrafo.
 
-p.innerText = 'Meu Texto!';
+p.innerText = 'Meu Texto!'; //mudando o texto do paragrafo.
 
-p.style.background ='red';
+p.style.background ='red'; //muda o background para vermelho (referente ao paragrafo)
 
 console.log(p);
 
 
 const cores = ['blue', 'red', 'yellow', 'purple'];
 
-for (i=0, c = 0; i<=100 ; i++, c++){
+for (i=0, c = 0; i<=100 ; i++, c++){ //estrutura de repetição
 
     p.style.background = cores [c];
 
-    console.log(c + ': ' + p.style.background);
+    console.log(c + ': ' + p.style.background); 
 
     if(c >= 4) c = -1;
 
