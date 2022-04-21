56e2437369a5085261e74bf98832d230b27f29be
ROBSON FERREIRA HERCULANO JUNIOR <robson.fhjunior@senacsp.edu.br>
2022-04-20 22:08:26 -0300

    LocalStorage

---
+++ listaDeTarefas.js
@@ -0,0 +1,142 @@
+const FORMULARIO = document.querySelector('#form-tarefa');
+const LISTA = document.querySelector('.collection');
+const BTN_LIMPAR = document.querySelector('.limpar-tarefa');
+const FILTRO = document.querySelector('#filtro');
+const CAMPO_TAREFA = document.querySelector('#tarefa');
+
+function carregaMonitorDeEventos() {
+
+    //eventos
+    FORMULARIO.addEventListener('submit', adicionarTarefa);
+    LISTA.addEventListener('click', apagarTarefa);
+    BTN_LIMPAR.addEventListener('click', apagaTodasTarefas);
+    FILTRO.addEventListener('keyup', filtraTarefa);
+
+}
+
+function pegaTarefa(){
+    let tarefas;
+
+    if(localStorage.getItem('tarefas') === null){
+        tarefas = [];
+    
+    }else{
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    }
+
+    tarefas.forEach(function(tarefa){
+        
+        //cria os novos elementos
+    const LI = document.createElement('li');
+    LI.ClassName = 'collection-item';
+    LI.appendChild(document.createTextNode(tarefa));
+    const A = document.createElement('a');
+    A.className = 'apaga-tarefa secondary-content';
+    const I = document.createElement('i');
+    I.className = 'fa fa-remove';
+    A.appendChild(I);
+    LI.appendChild(A);
+    LISTA.appendChild(LI);
+        //fim cria os novos elementos 
+    });
+}
+
+
+function filtraTarefa(evento){
+
+    const texto = evento.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(tarefa) {
+
+        const item = tarefa.firstChild.textContent;
+        
+        //-1 significa que não há o que esta sendo buscado
+        if(item.toLowerCase().indexOf(texto) != -1){
+            tarefa.style.display = 'block';
+        }else{
+
+            tarefa.style.display = 'nome';
+        }
+        
+    });
+
+
+}
+
+
+function apagaTodasTarefas(evento){
+
+    evento.preventDeFault();
+
+    LISTA.innerHTML = '';
+
+    if(evento.target.parentElement.classList.contains('apaga-tarefa')){
+        evento.target.parentElement.parentElement.remove();
+    
+    }
+}
+
+function apagarTarefa(evento){
+
+    if(evento.target.parentElement.classList.contains('apaga-tarefa')){
+        evento.target.parentElement.parentElement.remove();
+
+    }
+
+
+}
+
+function adicionarTarefa(evento){
+
+
+
+    evento.preventDeFault();
+    if(CAMPO_TAREFA.value === ''){//verifica se o campo está vazio
+
+        alert('Insira uma tarefa');
+   
+    }else{
+    
+
+  
+//cria os novos elementos
+
+    const LI = document.createElement('li');
+    LI.ClassName = 'collection-item';
+    LI.appendChild(document.createTextNode(CAMPO_TAREFA.value));
+    const A = document.createElement('a');
+    A.className = 'apaga-tarefa secondary-content';
+    const I = document.createElement('i');
+    I.className = 'fa fa-remove';
+    A.appendChild(I);
+    LI.appendChild(A);
+    //fim cria os novos elementos
+
+
+    LISTA.appendChild(LI);//adicionar a lista de tarefas.
+    
+    gravaTarefaNoLocalStorage(CAMPO_TAREFA);
+    CAMPO_TAREFA.value = '';
+
+    
+    }
+
+}
+
+function gravaTarefaNoLocalStorage(tarefa){
+
+    let tarefas;
+
+    if(localStorage.getItem('tarefas') === null){
+        tarefas = [];
+    }else{
+
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    }
+    tarefas.push(tarefa.value);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+
+}
+
+carregaMonitorDeEventos();
--- localStorage.js
+++ localStorage.js
@@ -1,10 +1,58 @@
-const OBJ = {nome: "Bono", 
-            idade: 37,
-            fazCompra: function(){
-                console.log('comprou alguma coisa');
-            
-        }};
+// De objeto JSON para String
 
- console.log(OBJ);
+const OBJ = {nome: "Bono", idade: 37};
 
- OBJ.fazCompra();
+console.log( JSON.stringify(OBJ) );
+
+//De string para objeto JSON
+
+const STR = '{nome": "Bono", "idade": 37}'; 
+
+console.log( JSON.parse(STR) );
+
+localStorage.setItem('nome', 'Bono');
+
+const NOME = localStorage.getItem('nome');
+
+console.log(NOME);
+
+sessionStorage.setItem('idade', '37');
+
+const IDADE = sessionStorage.getItem('idade');
+
+console.log(IDADE);
+
+//recupera valor do input preenchido pelo usuario e adiciona no storage
+document.querySelector('input.btn').addEventListener('click', function(e){
+
+    //previnir o comportamento padrão do botão
+    e.preventDefault();
+
+    const TAREFA = document.querySelector('#task').value;
+    
+  let tarefas;
+
+  //verificando no localstorage se existe o item tarefas.
+  if(localStorage.getItem('tarefas') === null){
+
+    tarefas = [];
+
+  }else{
+
+    tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+  }
+
+  tarefas.push(TAREFA);
+
+  localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+  console.log(tarefas);
+
+
+  document.getElementById('task').value = '';
+
+
+});
+
+//Para sessiomStorage é só trocar todos localstorage para sessionStorage
