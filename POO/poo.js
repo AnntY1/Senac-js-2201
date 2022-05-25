af7b57f7c45a1374ff065ced37caf52f71ff1b01
ROBSON FERREIRA HERCULANO JUNIOR <robson.fhjunior@senacsp.edu.br>
2022-05-25 20:10:04 -0300

    aula/25-05-22

--- POO/poo.js
+++ POO/poo.js
@@ -1,79 +1,120 @@
-/*class Usuario {
-    
-    //Quando instancia a classe ele é executado automaticamente
-    
-    constructor() 
-    {
-        this.nome = 'Bono';
-    }
+class Calculadora {
 
-    listar()
-    
-    {
-       this.mostraMensagem();
-    }
-
-    editar()
-    
-    {
-        console.log('editar Usuario');
-    }
-
-    cadastrar(){
-
-        console.log('cadastro feito');
-
-    }
-    mostraMensagem(){
-        alert('Mensagem para usuario');
-  
-    }
-}
-
-
-let user = new Usuario;
-let luiz = new Usuario;
-let bono = new Usuario;
-
-
-user.cadastrar();
-user.listar();
-console.log(user.nome);
-
-
-
-*/
-
-//exercicio calculadora
-
-class calculadora {
     constructor()
     {
-        document.getElementById('calc').addEventListener('click', this.calcula());
-
+        const operacao = document.getElementById('operacao').value;
+        
+        try{
+            this.calcula(operacao);
+        }catch(e){
+            alert('Escolha uma operação');
+        }
     }
 
-    calcula()
+    calcula(operacao)
     {
-        const n1 = document.getElementById('num1').value;
+        const n1 = document.getElementById('num1').value; 
         const n2 = document.getElementById('num2').value;
 
-        const resultado = this.som(n1, n2);
+        let resultado;
+
+        switch(operacao){
+            case 'soma':
+                resultado = this.soma(n1, n2);
+                break;
+            case 'subtracao':
+                resultado = this.subtracao(n1, n2);
+                break;
+            default:
+                throw SyntaxError('Operação não disponível');
+        }
+
         this.exibeResultado(resultado);
     }
+
     soma(nm1, nm2)
     {
         let n1 = Number(nm1);
         let n2 = Number(nm2);
-        
 
         return n1 + n2;
     }
+
+    subtracao(nm1, nm2)
+    {
+        let n1 = Number(nm1);
+        let n2 = Number(nm2);
+
+        return n1 - n2;
+    }    
 
     exibeResultado(res)
     {
         document.getElementById('resultado').innerText = res;
     }
+    
+    static metodoStatico()
+    {
+        console.log('Executou');
+    }
 }
 
-new calculadora;
+class CalculadoraAvancada extends Calculadora{
+
+    constructor(){
+        super();
+    }
+
+    calcula(operacao)
+    {
+        const n1 = document.getElementById('num1').value; 
+        const n2 = document.getElementById('num2').value;
+
+        let resultado;
+
+        switch(operacao){
+            case 'soma':
+                resultado = this.soma(n1, n2);
+                break;
+            case 'subtracao':
+                resultado = this.subtracao(n1, n2);
+                break;
+            case 'divisao':
+                resultado = this.divisao(n1, n2);
+                break;
+            case 'multiplicacao':
+                resultado = this.multiplicacao(n1, n2);
+                break;
+            default:
+                throw SyntaxError('Operação não disponível');
+        }
+
+        this.exibeResultado(resultado);
+    }
+
+    multiplicacao(nm1, nm2)
+    {
+        let n1 = Number(nm1);
+        let n2 = Number(nm2);
+
+        return n1 * n2;
+    }
+
+    divisao(nm1, nm2)
+    {
+        let n1 = Number(nm1);
+        let n2 = Number(nm2);
+
+        return n1 / n2;
+    }
+}
+
+document.getElementById('calc')
+            .addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    Calculadora.metodoStatico();
+
+    const obj = new CalculadoraAvancada;
+});
