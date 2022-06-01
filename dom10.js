c10f9fe48b868943319cb0fe284c32d1bafee26a
Robson Ferreira <99373533+RobsonFH@users.noreply.github.com>
2022-06-01 13:50:20 -0300

    Update dom10.js

--- dom10.js
+++ dom10.js
@@ -1,18 +1,5 @@
-//Quando clico no elemento mais interno em uma cadeia de elentos com
-//Event Listener, todos eventos dos elementos mais externos são acionados
-
-document.querySelector('.card-title').addEventListener('click', function(e){
-
-    console.log('Titulo');
+document.body.addEventListener('click', function(e){
+    if(e.target.parentElement.classList.contains('delete-item') === true){  //Se o if for true ele deleta o elemento pai do elemento pai, daquele que está sendo chamado.
+        e.target.parentElement.parentElement.remove();
+    };
 });
-
-document.querySelector('.card-content').addEventListener('click', function(e){
-
-    console.log('Form');
-});
-
-document.querySelector('.card').addEventListener('click',function(e){
-
-    console.log('Tudo');
-});
-
